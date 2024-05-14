90bcd90933cc5770b4ac6c508c50d8b63236649c
DanDamgaard <65528625+DanDamgaard@users.noreply.github.com>
2024-05-14 11:51:36 +0200

    init

---
+++ program/Class/Pages.cs
@@ -0,0 +1,20 @@
+﻿using program.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program.Class
+{
+    internal class Pages
+    {
+        private static LoginPage loginPage = null;
+        public static LoginPage getLoginPage()
+        {
+            if (loginPage == null)
+                loginPage = new LoginPage();
+            return loginPage;
+        }
+    }
+}
